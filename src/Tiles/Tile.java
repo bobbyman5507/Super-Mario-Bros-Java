cdd35a7881d9df51b09ea5bad1154637b2e26f54
rober <robert.orbin@gmail.com>
2025-02-17 17:24:19 -0500

    Initial implementation of tile superclass

--- src/Tiles/Tile.java
+++ src/Tiles/Tile.java
@@ -1,8 +1,34 @@
 package Tiles;
 
+import java.awt.image.BufferedImage;
+
+
 
 public abstract class Tile
 {
-    public
+    final private int xSize;
+    final private int ySize;
+    final private BufferedImage mainImg;
 
+    public Tile(int xSize, int ySize, BufferedImage mainImg)
+    {
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.mainImg = mainImg;
+    }
+
+    public int getXSize()
+    {
+        return this.xSize;
+    }
+
+    public int getYSize()
+    {
+        return this.ySize;
+    }
+
+    public BufferedImage getMainImg()
+    {
+        return this.mainImg;
+    }
 }

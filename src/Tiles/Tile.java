6f2432abd22d9a1b271d260453ef3707fb7e7243
Bobbyman5506 <65382589+bobbyman5507@users.noreply.github.com>
2025-02-18 02:30:19 -0500

    Made tile more generic for better implementation

--- src/Tiles/Tile.java
+++ src/Tiles/Tile.java
@@ -1,20 +1,37 @@
 package Tiles;
 
-import java.awt.image.BufferedImage;
+import java.awt.*;
 
 
-
+/**
+ * Abstract tile class for implementing new
+ * types of tiles for rendering
+ *
+ * @author Bobby Orbin
+ */
 public abstract class Tile
 {
+    //Constants
+    public static final int TILE_SIZE = 32;
+    public static final int Y_BUFFER = 48;
+
+
     final private int xSize;
     final private int ySize;
-    final private BufferedImage mainImg;
+    final private boolean collision;
 
-    public Tile(int xSize, int ySize, BufferedImage mainImg)
+
+    /**
+     * Abstract tile class for implementation
+     * @param xSize the number of pixels of the tile in the X direction
+     * @param ySize the number of pixels of the tile in the Y direction
+     * @param collision can mario collide with the tile
+     */
+    public Tile(int xSize, int ySize, boolean collision)
     {
         this.xSize = xSize;
         this.ySize = ySize;
-        this.mainImg = mainImg;
+        this.collision = collision;
     }
 
     public int getXSize()
@@ -27,8 +44,5 @@ public abstract class Tile
         return this.ySize;
     }
 
-    public BufferedImage getMainImg()
-    {
-        return this.mainImg;
-    }
+    public abstract void draw(Graphics2D g2, int tileX, int tileY);
 }

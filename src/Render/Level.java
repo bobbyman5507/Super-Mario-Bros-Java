d5ac7fdd8b18d5dc0f90f88d03bbe326f05bdd54
rober <robert.orbin@gmail.com>
2025-02-25 15:12:21 -0500

    small work on level creation

--- src/Render/Level.java
+++ src/Render/Level.java
@@ -14,7 +14,10 @@ import java.util.Scanner;
 public class Level
 {
     private final String levelName;
+    private final int maxLength;
+    private final int sublevel;
     private final HashMap<Integer, Tile> ColorPalette;
+    private final int[][] level;
 
     public Level(String filename) throws FileNotFoundException
     {
@@ -23,13 +26,13 @@ public class Level
         /*
         Level file format:
         Line 1:
-        <Level Name>$<Color Palette>
+        <Level Name>$<sublevel>$<maxLength>$<Color Palette>
 
         Lines 2-16: level data from time row(0) to bottom (14)
         formatting:
         ! - Separator
-        #<ID># - Single tile
-        %<ID>,<Length>%
+        #<ID> - Single tile
+        %<ID>,<Length>
         */
         String currLine;
         ArrayList<String> splitLine = new ArrayList<>();
@@ -39,11 +42,30 @@ public class Level
         splitLine = (ArrayList<String>) Arrays.asList(currLine.split("$"));
 
         this.levelName = splitLine.getFirst();
+        this.sublevel = Integer.parseInt(splitLine.get(1));
+        this.maxLength = Integer.parseInt(splitLine.get(2));
+
+
 
         //loading line into array
 
+        int currPos = 0;
+        char detChar;
+
         for(int i = 1; i<16; i++)
         {
+            //getting next line
+            currLine = in.nextLine();
+            splitLine = (ArrayList<String>) Arrays.asList(currLine.split("!"));
+
+            //loop to get to end
+            for (int j = 0; i < splitLine.size(); i++)
+            {
+                detChar = splitLine.get(i).charAt(0);
+
+                if(detChar == '#')
+            }
+
 
         }
     }
